e6e32757237e9274837b6d742b0d11a3f864407e
aj ancheta and dustin herboldshimer <dustin@>
2019-06-10 15:29:56 -0700

    add toggle for done property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,5 +18,21 @@ export class AppComponent {
     new Task('Begin brainstorming possible JavaScript group projects'),
     new Task('Add README file to last few Angular repos on GitHub')
   ]
+
+  toggleDone(event){
+    console.log(event);
+    for(let i = 0; i < this.tasks.length; i++){
+      if(this.tasks[i].description == event.target.textContent){
+        if(this.tasks[i].done == true) {
+          this.tasks[i].done = false;
+          event.target.classList.remove('isDone');
+        } else {
+          this.tasks[i].done = true;
+          event.target.classList.add('isDone');
+        }
+      }
+    }
+    console.log(this.tasks);
+  }
 }
 
